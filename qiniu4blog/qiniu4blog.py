1bdec044b835e208dcee8e87d234174ca8f3cf7f
wzyuliyang <wzyuliyang911@gmail.com>
2015-11-15 14:54:58 +0800

    使用 watchdog 和多线程重构代码

--- qiniu4blog/qiniu4blog.py
+++ qiniu4blog/qiniu4blog.py
@@ -1,74 +1,107 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import os, time, sys, ConfigParser,platform,urllib
-import qiniu
+import os,time,sys,ConfigParser,platform,urllib,qiniu,pyperclip,signal,threading
 from mimetypes import MimeTypes
-import pyperclip
 from os.path import expanduser
-import signal
+from watchdog.observers import Observer
+from watchdog.events import PatternMatchingEventHandler
+
+#使用watchdog 监控文件夹中的图像
+class MyHandler(PatternMatchingEventHandler):
+    patterns = ["*.jpeg", "*.jpg","*.png","*.bmp","gif"]
+    ignore_directories = True
+    case_sensitive = False
+    def process(self, event):
+        if event.event_type == 'created':
+            threadCreated = myThread(2, "created-Thread", event.src_path , 1)
+            threadCreated.start()
+        if event.event_type == 'modified':
+            threadModified = myThread(2, "modified-Thread", event.src_path, 1)
+            threadModified.start()
+
+    def on_modified(self, event):
+        self.process(event)
+
+    def on_created(self, event):
+        self.process(event)
+
+#使用多线程上传
+class myThread(threading.Thread):
+    def __init__(self, threadID, name, filePath, mode):
+        threading.Thread.__init__(self)
+        self.threadID = threadID
+        self.name = name
+        self.filePath = filePath
+        self.mode = mode
+
+    def run(self):
+        threadLock.acquire()
+        job(self.filePath,self.mode)
+        threadLock.release()
+
+#上传图像、复制到粘贴板、写到文件
+def job(file,mode):
+    if mode == 1:
+        url = upload_with_full_Path(file)
+    if mode == 2:
+        url = upload_with_full_Path_cmd(file)
+    pyperclip.copy(url)
+    pyperclip.paste()
+    print url
+    with open('Markdown-格式外链.txt', 'a') as f:
+        f.write(url+'\n')
 
 
-homedir = expanduser("~")                           #get home dir path
-config = ConfigParser.RawConfigParser()             #read config
-config.read(homedir+'/qiniu.cfg')
+homedir = expanduser("~")  # 获取用户主目录
+config = ConfigParser.RawConfigParser()
+config.read(homedir + '/qiniu.cfg') #读取配置文件
 mime = MimeTypes()
+threadLock = threading.Lock()
 
+#优雅退出
 def exit_gracefully(signum, frame):
     signal.signal(signal.SIGINT, original_sigint)
     try:
         if raw_input("\nReally quit? (y/n)> ").lower().startswith('y'):
             sys.exit(1)
-
     except KeyboardInterrupt:
         print("Ok ok, quitting")
         sys.exit(1)
-
-    # restore the exit gracefully handler here
     signal.signal(signal.SIGINT, exit_gracefully)
+
 
 original_sigint = signal.getsignal(signal.SIGINT)
 signal.signal(signal.SIGINT, exit_gracefully)
 
-
 try:
-    bucket = config.get('config', 'bucket')         #set  bucket
-    accessKey = config.get('config', 'accessKey')   #set  accessKey
-    secretKey = config.get('config', 'secretKey')   #set  secretKey
-    path_to_watch = config.get('config', 'path_to_watch')   #set image root dir
-    enable = config.get('custom_url','enable')        #set custom_url
+    bucket = config.get('config', 'bucket')  # 设置  bucket
+    accessKey = config.get('config', 'accessKey')  # 设置  accessKey
+    secretKey = config.get('config', 'secretKey')  # 设置  secretKey
+    path_to_watch = config.get('config', 'path_to_watch')  # 设置   监控文件夹
+    enable = config.get('custom_url', 'enable')  # 设置自定义使能 custom_url
     if enable == 'false':
         print 'custom_url not set'
     else:
-        addr = config.get('custom_url','addr')
+        addr = config.get('custom_url', 'addr')
 except ConfigParser.NoSectionError, err:
     print 'Error Config File:', err
 
-#set character coding
+
+#设置编码
 def setCodeingByOS():
     if 'cygwin' in platform.system().lower():
-        CODE = 'GBK'
+        return 'GBK'
     elif os.name == 'nt' or platform.system() == 'Windows':
-        CODE = 'GBK'
+        return 'GBK'
     elif os.name == 'mac' or platform.system() == 'Darwin':
-        CODE = 'utf-8'
+        return 'utf-8'
     elif os.name == 'posix' or platform.system() == 'Linux':
-        CODE = 'utf-8'
-    return  CODE
+        return 'utf-8'
 
-#set clipboard
-def set_clipboard(url_list):
-	for url in url_list:
-		pyperclip.copy(url)
-	spam = pyperclip.paste()
-
-
-#print response from qiniu server
+#处理七牛返回结果
 def parseRet(retData, respInfo):
     if retData != None:
-        print("Upload file success!")
-        print("Hash: " + retData["hash"])
-        print("Key: " + retData["key"])
         for k, v in retData.items():
             if k[:2] == "x:":
                 print(k + ":" + v)
@@ -79,9 +112,9 @@ def parseRet(retData, respInfo):
                 print(k + ":" + str(v))
     else:
         print("Upload file failed!")
-        print("Error: " + respInfo.text_body)
 
-#upload file style1
+
+#上传文件方式 1
 def upload_without_key(bucket, filePath, uploadname):
     auth = qiniu.Auth(accessKey, secretKey)
     upToken = auth.upload_token(bucket, key=None)
@@ -89,7 +122,8 @@ def upload_without_key(bucket, filePath, uploadname):
     retData, respInfo = qiniu.put_file(upToken, key, filePath, mime_type=mime.guess_type(filePath)[0])
     parseRet(retData, respInfo)
 
-#upload file style2
+
+#上传文件方式 2
 def upload_with_full_Path(filePath):
     if platform.system() == 'Windows':
         fileName = "/".join("".join(filePath.rsplit(path_to_watch))[1:].split("\\"))
@@ -97,80 +131,40 @@ def upload_with_full_Path(filePath):
         fileName = "".join(filePath.rsplit(path_to_watch))[1:]
     upload_without_key(bucket, filePath, fileName.decode(setCodeingByOS()))
     if enable == 'true':
-        url = addr + urllib.quote(fileName.decode(setCodeingByOS()).encode('utf-8'))
+        return addr + urllib.quote(fileName.decode(setCodeingByOS()).encode('utf-8'))
     else:
-        url = 'http://' + bucket + '.qiniudn.com/' + urllib.quote(fileName.decode(setCodeingByOS()).encode('utf-8'))
-    return url
+        return 'http://' + bucket + '.qiniudn.com/' + urllib.quote(fileName.decode(setCodeingByOS()).encode('utf-8'))
 
-#upload file style3
+
+#上传文件方式 3
 def upload_with_full_Path_cmd(filePath):
     if platform.system() == 'Windows':
-        filePath = "/".join((filePath.split("\\")))
-        fileName = os.path.basename(filePath)
+        fileName = os.path.basename("/".join((filePath.split("\\"))))
     else:
         fileName = os.path.basename(filePath)
     upload_without_key(bucket, filePath, fileName.decode(setCodeingByOS()))
     if enable == 'true':
-        url = addr + urllib.quote(fileName.decode(setCodeingByOS()).encode('utf-8'))
+        return addr + urllib.quote(fileName.decode(setCodeingByOS()).encode('utf-8'))
     else:
-        url = 'http://' + bucket + '.qiniudn.com/' + urllib.quote(fileName.decode(setCodeingByOS()).encode('utf-8'))
-    return url
-
-#get all file path on root image dir
-def get_filepaths(directory):
-    file_paths = []  # List which will store all of the full filepaths.
-    for root, directories, files in os.walk(directory):
-        for filename in files:
-            # Join the two strings in order to form the full filepath.
-            filepath = os.path.join(root, filename)
-            file_paths.append(filepath)  # Add it to the list.
-    return file_paths  # Self-explanatory.
-
-
+        return 'http://' + bucket + '.qiniudn.com/' + urllib.quote(fileName.decode(setCodeingByOS()).encode('utf-8'))
 
 
 def main():
     if len(sys.argv) > 1:
         url_list = []
         for i in sys.argv[1:]:
-            url = upload_with_full_Path_cmd(i)
-            url_list.append(url)
-        with open('image_markdown.txt', 'a') as f:
-            for url in url_list:
-                image = '![' + url + ']' + '(' + url + ')' + '\n'
-                print url,'\n'
-                f.write(image)
-        print "\nNOTE: save image url [markdown] in image_markdwon.txt"
-        set_clipboard(url_list)
-        return
+            myThread(2, "Thread", i,2).start()
+        sys.exit(-1)
     print "running ... ... \nPress Ctr+C to Stop"
-    before =  get_filepaths(path_to_watch)
-    while 1:
-        time.sleep(1)
-        after = get_filepaths(path_to_watch)
-        added = [f for f in after if not f in before]
-        removed = [f for f in before if not f in after]
-        if added:
-            print "Added Files: ", ", ".join(added)
-            url_list = []
-            for i in added:
-                url = upload_with_full_Path(i)
-                url_list.append(url)
-            with open('image_markdown.txt', 'a') as f:
-                for url in url_list:
-                    image = '![' + url + ']' + '(' + url + ')' + '\n\n'
-                    print url,'\n\n'
-                    f.write(image)
-            print "\nNOTE: save image url [markdown] in image_markdwon.txt"
-            set_clipboard(url_list)
-        if removed:
-            print "Removed Files: ", ", ".join(removed)
-            print  removed
-        before = after
-
-
+    observer = Observer()
+    observer.schedule(MyHandler(), path=path_to_watch if path_to_watch else '.', recursive=True)
+    observer.start()
+    try:
+        while True:
+            time.sleep(0.1)
+    except KeyboardInterrupt:
+        observer.stop()
+    observer.join()
 
 if __name__ == "__main__":
     main()
-
-
