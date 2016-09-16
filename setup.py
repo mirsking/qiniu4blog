30dbf85220a68edd9970d01b6cc974be658391a1
mirsking@gmail.com <mirsking>
2016-09-16 10:25:21 +0800

    use custom cmd, not work now

--- setup.py
+++ setup.py
@@ -2,17 +2,17 @@
 from setuptools import setup, find_packages
 import sys, os
 
-version = '1.4.0'
+version = '1.4.2'
 
-setup(name='qiniu4blog',
+setup(name='qiniu4blog_mk',
       version=version,
       description="写博客用的七牛图床",
-      long_description="""写博客用的七牛图床""",
+      long_description="""The author of the origin version of this package is wzyuliyang, the current version is modified by mirsking""",
       classifiers=[],
       keywords='python qiniu',
-      author='wzyuliyang',
-      author_email='wzyuliyang911@gmail.com',
-      url='https://github.com/wzyuliyang/qiniu4blog',
+      author='wzyuliyang, mirsking',
+      author_email='wzyuliyang911@gmail.com, mirsking@gmail.com',
+      url='https://github.com/wzyuliyang/qiniu4blog, https://github.com/mirsking/qiniu4blog',
       license='MIT License',
       packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
       include_package_data=True,
@@ -20,11 +20,11 @@ setup(name='qiniu4blog',
       install_requires=[
         'qiniu',
 		'pyperclip',
-    'watchdog',
+        'watchdog',
       ],
       entry_points={
         'console_scripts':[
-            'qiniu4blog = qiniu4blog.qiniu4blog:main'
+            'qiniu4blog_mk = qiniu4blog.qiniu4blog:main'
         ]
       },
 )
